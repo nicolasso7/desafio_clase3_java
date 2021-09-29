9cbc881eef34c31b67ba8bba8b0c99964c1df3f6
nicolasso <nicolassocac@gmmail.com>
2021-09-29 08:41:44 -0300

    nuevos cambios en app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-// Requisitos para viajar al extranjero
+let color = prompt("Ingresa color de Parlante");
 
-const edad = Number(prompt("Ingrese su edad"));
-const vacunas = Number(prompt("Cuantas dosis de vacuna tiene aplicadas?"));
+    switch ((parlante = color)) {
+        case "negro":
+        alert("hay en stock");
+        break;
+        case "blanco":
+        alert("no hay stock");
+        break;
+        case "color madera":
+        alert("ultimo en stock");
+        break;
+        default:
+           alert("ESE COLOR NO ESTA EN STOCK")
 
-const destino = confirm("Quiere viajar dentro del país?");
-/* true => nacional
-   false => extranjero */
-   
-const aprobacion = (!destino && edad >=18 && vacunas >=1) || (destino);
-
-if (aprobacion) {
-    alert("Su viaje ha sido autorizado")
-} else {
-    alert ("No cumple los requisitos para viajar al extranjero")
-}
+    }
